7e6cf87e6e8b4638c7bfdaa1567d64b3e88c22ad
Joey Stox <joestox@gmail.com>
2023-03-16 02:21:14 -0300

    Update matrix dependency again

---
+++ test/LineGeometrySpec.purs
@@ -0,0 +1,10 @@
+module Test.LineGeometrySpec where
+
+import Prelude
+import Test.Spec (Spec, describe, it)
+
+lineGeometrySpec :: Spec Unit
+lineGeometrySpec = do
+  describe "LineGeometrySpec" do
+    it "should divide" do
+      pure unit
