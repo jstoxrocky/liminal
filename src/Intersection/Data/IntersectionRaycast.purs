0fb30a71ff9cedbc087ec36d26b77525a1fff2eb
Joey Stox <joestox@gmail.com>
2023-04-16 14:45:45 -0300

    Add basic Object3D class instances to IntersectionRaycast

--- src/Intersection/Data/IntersectionRaycast.purs
+++ src/Intersection/Data/IntersectionRaycast.purs
@@ -4,6 +4,10 @@ import Prelude
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
 import TransformationMatrix.Data.Vector3 (Vector3)
+import Liminal.Class.HasUuid (class HasUuid, getUuid, setUuid)
+import Liminal.Class.HasMatrix (class HasMatrix, getMatrix, setMatrix, getPosition, setPosition)
+import Liminal.Class.HasInverse (class HasInverse, getInverse, setInverse)
+import Liminal.Class.HasAxisAlignedBoundingBox (class HasAxisAlignedBoundingBox, getAxisAlignedBoundingBox)
 
 data IntersectionRaycast a = IntersectionRaycast 
   { distance :: Number
@@ -28,6 +32,23 @@ instance ordIntersectionRaycast :: Ord a => Ord (IntersectionRaycast a) where
     if distance1 < distance2 then LT
     else if distance1 == distance2 then compare object1 object2
     else GT
+
+instance hasUuidIntersectionRaycast :: HasUuid a => HasUuid (IntersectionRaycast a) where
+  getUuid (IntersectionRaycast { object }) = getUuid object
+  setUuid uuid (IntersectionRaycast attrs@{ object }) = IntersectionRaycast attrs { object = setUuid uuid object }
+
+instance hasMatrixIntersectionRaycast :: HasMatrix a => HasMatrix (IntersectionRaycast a) where
+  getMatrix (IntersectionRaycast { object }) = getMatrix object
+  setMatrix matrix (IntersectionRaycast attrs@{ object }) = IntersectionRaycast attrs { object = setMatrix matrix object }
+  getPosition (IntersectionRaycast { object }) = getPosition object
+  setPosition v3 (IntersectionRaycast attrs@{ object }) = IntersectionRaycast attrs { object = setPosition v3 object }
+
+instance hasInverseIntersectionRaycast :: HasInverse a => HasInverse (IntersectionRaycast a) where
+  getInverse (IntersectionRaycast { object }) = getInverse object
+  setInverse inverseMatrix (IntersectionRaycast attrs@{ object }) = IntersectionRaycast attrs { object = setInverse inverseMatrix object }
+
+instance hasAxisAlignedBoundingBoxIntersectionRaycast :: HasAxisAlignedBoundingBox a => HasAxisAlignedBoundingBox (IntersectionRaycast a) where
+  getAxisAlignedBoundingBox (IntersectionRaycast { object }) = getAxisAlignedBoundingBox object
 
 getRaycastHoveredObject
   :: forall a
